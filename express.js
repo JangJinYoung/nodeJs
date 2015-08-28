3549c4ab64a97a2b83c7a7584fa71a4c81a1a3df
JangJinYoung <expitly90@naver.com>
2015-08-29 02:49:52 +0900

    module

---
+++ express.js
@@ -0,0 +1,18 @@
+﻿var http = require('http');
+var express = require('express');
+var cookieParser = require('cookie-parser')
+var app = express();
+
+app.use(cookieParser());
+app.get('/getCookie', function(request,response){
+	response.send(request.cookies);
+}); 
+app.get('/setCookie', function(request,response){
+	response.cookie('string', 'cookie');
+
+	response.redirect('/getCookie');
+}); 
+
+http.createServer(app).listen(52273,function(){
+	console.log('server running.....');
+}); 
