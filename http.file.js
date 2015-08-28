754b0330c988298c7212f751fb4eb83a312f8306
JangJinYoung <expitly90@naver.com>
2015-08-28 15:35:03 +0900

    html파일 사용

---
+++ http.file.js
@@ -0,0 +1,11 @@
+﻿var fs = require('fs');
+var http = require('http');
+
+http.createServer(function(request, response){
+	fs.readFile('HTMLPage.html', function(error, data){
+		response.writeHead(200, {'Content-Type':'text/html'});
+		response.end(data);
+	});
+}).listen(52273,function(){
+	console.log('Server Running..');
+});
